96ae53fa503c8476dc488b4d9c8d00afcb3d3d18
Ankit <ankitsolanki61375@gmail.com>
2023-07-15 21:28:10 +0530

    first commit

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import classes from './header.module.css'
+import { Link } from 'react-router-dom';
+function Header() {
+
+  const user = {
+    name:'ankit',
+  };
+
+  const cart = {
+    totalCount:10,
+  }
+  const logout = () => {}
+  return (
+    <header className ={classes.header}>
+        <div className={classes.container}>
+            <Link to="/" className={classes.logo}>
+                Food mine!
+            </Link>
+            <nav>
+                <ul>
+                    {
+                        user?
+                        <li className={classes.menu_container}>
+                            <Link to="/profile">{user.name}</Link>
+                            <div className={classes.menu}>
+                                <Link to ="/profile">Profile</Link>
+                                <Link to ="/orders">Orders</Link>
+                                <a onClick={logout}>Logout</a>
+
+                            </div>
+                        </li>:
+                        <Link to="/login">Login</Link>
+
+                    }
+                    <Link to="/cart">
+                        Cart
+                        {cart.totalCount > 0 && <span className={classes.cart_count}>{cart.totalCount}</span>}
+                    </Link>
+                </ul>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
